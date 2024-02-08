fb06394ec8824476b9d46e6f363697c14f0f1ce0
shanejohnson <shane.john316@gmail.com>
2024-02-08 12:04:39 -0600

    Added logic to allow users to enter hex codes for the shapeColor and textColor. Also added logic to make sure users can't enter more than 3 characters for the text

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Square, Triangle } = require('./lib/shapes');
 
+function validateTextLength(value) {
+    if (value.length > 3) {
+        return 'Please enter no more than 3 characters.';
+    }
+    return true;
+}
+
 const questions = [
     {
         type: 'list',
@@ -17,7 +24,8 @@ const questions = [
     {
         type: 'input',
         name: 'text',
-        message: 'Please enter up to 3 characters'
+        message: 'Please enter up to 3 characters',
+        validate: validateTextLength
     },
     {
         type: 'input',
@@ -26,14 +34,25 @@ const questions = [
     }
 ]
 
+function isValidColor(color) {
+    return /^#[0-9a-f]{3,6}$/i.test(color) || /(red|green|blue|black|white|yellow|purple|orange|pink|gray|silver|gold|teal|aqua|lime|maroon|olive|navy)/i.test(color);
+}
+
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
             const ShapeClass = answers.shape === 'Circle'
                 ? Circle
                 : answers.shape === 'Triangle'
-                ? Triangle
-                : Square;
+                    ? Triangle
+                    : Square;
+
+            if (!isValidColor(answers.shapeColor)) {
+                throw new Error('Invalid shape color');
+            }
+            if (!isValidColor(answers.textColor)) {
+                throw new Error('Invalid text color');
+            }
 
             const shape = new ShapeClass(answers.shapeColor, answers.text, answers.textColor)
             const svg = shape.getSVG();
